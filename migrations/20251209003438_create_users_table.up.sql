a7af48a3650f1d07ead549c90b51cd29e5d83fce
Arbath <arbath@teknohole.com>
2026-01-03 08:21:54 +0700

    merge user migration

--- migrations/20251209003438_create_users_table.up.sql
+++ migrations/20251209003438_create_users_table.up.sql
@@ -9,6 +9,15 @@ CREATE TABLE users (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+-- Add up migration script here
+CREATE TABLE refresh_tokens (
+    token TEXT PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    expires_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens(user_id);
+
 -- updated_at auto now
 CREATE OR REPLACE FUNCTION update_updated_at_column()
 RETURNS TRIGGER AS $$
