c715a6a0b05503d8b94774098208ddcbcb0ec404
Arbath <arbath@teknohole.com>
2026-01-21 10:24:09 +0700

    add: api keys authorization

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc};
+use sqlx::FromRow;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
@@ -33,4 +34,21 @@ pub struct LoginRes {
 #[derive(Deserialize)]
 pub struct RefreshTokenReq {
     pub refresh_token: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct ApiKey {
+    pub id: i32, 
+    pub user_id: i32, 
+    pub name: String,
+    pub descriptions: Option<String>,
+    pub key: Option<String>,
+    pub expires_at: Option<DateTime<Utc>>,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(FromRow)]
+pub struct CheckApiKey {
+    pub user_id: i32, 
+    pub expires_at: Option<DateTime<Utc>>,
 }
